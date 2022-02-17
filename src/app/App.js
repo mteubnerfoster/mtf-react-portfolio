6c6dbf71b43109cb480719b3613fa638d815b880
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2022-02-16 22:13:22 -0800

    added imports and routers to src/app

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  useLocation,
+  withRouter,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AppRoutes from "./routes";
+import Headermain from "../header";
+import AnimatedCursor from "react-animated-cursor";
+import { Socialicons } from "../components/socialicons";
+import "./App.css";
+
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children;
 }
 
-export default App;
+const ScrollToTop = withRouter(_ScrollToTop);
+
+export default function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="cursor__dot">
+        <AnimatedCursor
+          innerSize={15}
+          outerSize={15}
+          color="255, 255, 255"
+          outerAlpha={0.4}
+          innerScale={0.7}
+          outerScale={5}
+        />
+      </div>
+      <ScrollToTop>
+        <Headermain />
+        <AppRoutes />
+        <Socialicons />
+      </ScrollToTop>
+    </Router>
+  );
+}
